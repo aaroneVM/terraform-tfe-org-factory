0c50d9dd4d0086ad53b824c770610dcc957578a7
aarone <aarone@outlook.com.au>
2022-08-10 23:17:13 +1000

    init

--- examples/use-existing-org/main.tf
+++
@@ -1,7 +0,0 @@
-# Configure existing organization with module
-module "tfe_workspace" {
-  source = "../.."
-
-  organization_name = var.organization_name
-  config_file_path  = var.config_file_path
-}
